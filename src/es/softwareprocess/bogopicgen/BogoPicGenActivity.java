3065711660438a6147fd5cc101080fe3642f0544
uaLogan <lglin@cs.ualberta.ca>
2014-02-04 19:11:52 -0700

    It's working

--- src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
+++ src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
@@ -60,7 +60,8 @@ public class BogoPicGenActivity extends Activity {
 	Uri imageFileUri;
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
@@ -69,14 +70,12 @@ public class BogoPicGenActivity extends Activity {
         ImageButton button = (ImageButton)findViewById(R.id.TakeAPhoto);
         OnClickListener listener = new OnClickListener() {
             public void onClick(View v) {
-                //takeAPhoto();
             	setBogoPic();
             }
         }; 
         button.setOnClickListener(listener);
         
         Button acceptButton = (Button)findViewById(R.id.Accept);
-        
         acceptButton.setOnClickListener( new OnClickListener() {
             public void onClick(View v) {
             	processIntent(false);
@@ -84,68 +83,77 @@ public class BogoPicGenActivity extends Activity {
         });
         
         Button cancelButton = (Button)findViewById(R.id.Cancel);
-        
         cancelButton.setOnClickListener( new OnClickListener() {
             public void onClick(View v) {
             	processIntent(true);
             }        	
         });
-
-        
-        
-        
-		        
-        //takeAPhoto();
     }
 
    
 
     private Bitmap ourBMP;
-    private void setBogoPic() {
-    	Toast.makeText(this, "Generating Photo", Toast.LENGTH_LONG).show();
+    private void setBogoPic()
+    {
+    	//Toast.makeText(this, "Generating Photo", Toast.LENGTH_LONG).show();
 		ImageButton button = (ImageButton)findViewById(R.id.TakeAPhoto);
 		ourBMP = BogoPicGen.generateBitmap(400, 400);
 		button.setImageBitmap(ourBMP);
 	}
-    private File getPicturePath(Intent intent) {
+    private File getPicturePath(Intent intent)
+    {
         Uri uri = (Uri) intent.getExtras().get(MediaStore.EXTRA_OUTPUT);
         return new File(uri.getPath());
     }
 
     // call this to accept
-    private void processIntent(boolean cancel) {
+    private void processIntent(boolean cancel)
+    {
     	Intent intent = getIntent();
     	if (intent == null) {
     		return;
     	}
-    	try {
-    		if (intent.getExtras() != null) {    
-    			if (cancel) {
-    				Toast.makeText(this, "Photo Cancelled!", Toast.LENGTH_LONG).show();
-    				setResult(RESULT_CANCELED);
-    				finish();
-    				return;
-    			}
-    			File intentPicture = getPicturePath(intent);
-    			saveBMP(intentPicture, ourBMP);
-    			setResult(RESULT_OK);
-    		} else {
-    			Toast.makeText(this, "Photo Cancelled: No Reciever?", Toast.LENGTH_LONG).show();
-    			setResult(RESULT_CANCELED);
-    		}
-    	} catch (FileNotFoundException e) {
-    		Toast.makeText(this, "Couldn't Find File to Write to?", Toast.LENGTH_LONG).show();
-    		setResult(RESULT_CANCELED);    	
-    	} catch (IOException e) {
-    		Toast.makeText(this, "Couldn't Write File!", Toast.LENGTH_LONG).show();
-    		setResult(RESULT_CANCELED);
-    	}
+		try
+		{
+			if (intent.getExtras() != null)
+			{
+				if (cancel)
+				{
+					Toast.makeText(this, "Photo Cancelled!", Toast.LENGTH_LONG).show();
+					setResult(RESULT_CANCELED);
+					finish();
+					return;
+				}
+				File intentPicture = getPicturePath(intent);
+				saveBMP(intentPicture, ourBMP);
+				setResult(RESULT_OK);
+			}
+			else
+			{
+				Toast.makeText(this, "Photo Cancelled: No Reciever?", Toast.LENGTH_LONG).show();
+				setResult(RESULT_CANCELED);
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			Toast.makeText(this, "Couldn't Find File to Write to?", Toast.LENGTH_LONG).show();
+			setResult(RESULT_CANCELED);
+		}
+		catch (IOException e)
+		{
+			Toast.makeText(this, "Couldn't Write File!", Toast.LENGTH_LONG)
+					.show();
+			setResult(RESULT_CANCELED);
+		}
+		
     	finish();
     }
-    private void saveBMP( File intentPicture, Bitmap ourBMP) throws IOException, FileNotFoundException {
-    		OutputStream out = new FileOutputStream(intentPicture);
-    		ourBMP.compress(Bitmap.CompressFormat.JPEG, 75, out);
-    		out.close();
+    
+    private void saveBMP( File intentPicture, Bitmap ourBMP) throws IOException, FileNotFoundException
+    {
+		OutputStream out = new FileOutputStream(intentPicture);
+		ourBMP.compress(Bitmap.CompressFormat.JPEG, 75, out);
+		out.close();
     }
     
 
